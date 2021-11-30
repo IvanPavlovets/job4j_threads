7393786b11d73ec14cffa0da72751eb2e4127993
IvanPavlovets <ivan150287@gmail.com>
2021-11-30 13:35:56 +0700

    4. Скачивание файла с ограничением. [#144271] 1.2

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -10,48 +10,75 @@ import java.net.URL;
  * Для ограничения скорости скачивания, засекаеться время
  * скачивания 1024 байт - переменная startLoading.
  * Если время меньше указанного, то в условии выставляеться паузу за
- * счет Thread.sleep. Пауза вычисляеться - (speed - timeDelay).
+ * счет Thread.sleep.
  */
 public class Wget implements Runnable {
     private String url;
+    private String urlOut;
     private int speed;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, String urlOut, int speed) {
         this.url = url;
+        this.urlOut = urlOut;
         this.speed = speed;
     }
 
     /**
-     * кода для скачивания файла с задержкой.
+     * код для скачивания файла с задержкой до 1 мегабайта в секунду.
      */
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream out = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream out = new FileOutputStream(urlOut)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
+            long bytesWrited = 0;
+            long deltaTime;
             long startLoading = System.currentTimeMillis();
-            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                out.write(dataBuffer, 0, bytesRead);
-                Thread.sleep(1000);
-                long finishLoading = System.currentTimeMillis();
-                long timeDelay = finishLoading - startLoading;
-                if (speed > timeDelay) {
-                    Thread.sleep(speed - timeDelay);
+            do {
+                bytesRead = in.read(dataBuffer, 0, 1024);
+                bytesWrited = bytesWrited + bytesRead;
+                out.write(dataBuffer, 0, 1024);
+                if (bytesWrited >= speed) {
+                    deltaTime = System.currentTimeMillis() - startLoading;
+                    if (deltaTime < 1000) {
+                        Thread.sleep(1000 - deltaTime);
+                    }
                 }
-            }
+            } while (bytesRead != -1);
         } catch (Exception e) {
             Thread.currentThread().interrupt();
+            e.fillInStackTrace();
         }
     }
 
+    /**
+     * Для запуска программы нужно передать 3 аргумента
+     * url - url файла;
+     * urlOut - путь(url) на своей локальной машине, куда сохранить файд
+     * bytesPerSecond - пропускная способность. Байты в секунду.
+     * если ограничивать скорость до 1 мегабайта в секунду
+     * - это 1048576 байт в секунду.
+     * КБайт - 1024 байта.
+     * МБайт - 1024 килобайта.
+     * @param args
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws InterruptedException {
-        String url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
-        int speed = 2000;
+        validateArgsCount(args);
+        String url = args[0];
+        String urlOut = args[1];
+        int bytesPerSecond = Integer.parseInt(args[2]);
 
-        Thread wget = new Thread(new Wget(url, speed));
+        Thread wget = new Thread(new Wget(url, urlOut, bytesPerSecond));
         wget.start();
         wget.join();
 
+    }
+
+    private static void validateArgsCount(String[] args) {
+        if (args.length < 3) {
+            throw new IllegalArgumentException("there must be 3 arguments!");
+        }
     }
 }
