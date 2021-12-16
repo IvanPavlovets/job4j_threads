ed0f509e802d564610939e1fd2be7cdbd411f06f
IvanPavlovets <ivan150287@gmail.com>
2021-12-16 17:55:06 +0700

    2. Обеспечить остановку потребителя. [#66825 #240596] 1.1

--- src/main/java/ru/job4j/block/buffer/ParallelSearch.java
+++ src/main/java/ru/job4j/block/buffer/ParallelSearch.java
@@ -4,7 +4,8 @@ import ru.job4j.block.queue.SimpleBlockingQueue;
 
 /**
  * Механизм остановки потребителя, когда производитель закончил свою работу.
- * Остановка для consumer происходит по условию в цикле (окончание количесва вызовов очереди)
+ * Прерывание для consumer происходит по окончанию работы producer
+ * и consumer заканчивает свою работу как прерваный.
  * Что бы не происходила ситуация - когда нить производитель заканчивает свою работу,
  * потребители переходят в режим wait.
  */
@@ -12,6 +13,18 @@ public class ParallelSearch {
     public static void main(String[] args) {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
         int queueCall = 3;
+        final Thread consumer = new Thread(
+                () -> {
+                    try {
+                        while (!Thread.currentThread().isInterrupted()) {
+                            System.out.println(queue.poll());
+                        }
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+        );
+
         Thread producer = new Thread(
                 () -> {
                     try {
@@ -19,27 +32,15 @@ public class ParallelSearch {
                             queue.offer(i);
                             Thread.sleep(500);
                         }
+
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                         Thread.currentThread().interrupt();
                     }
+                    consumer.interrupt();
                 }
         );
         producer.start();
-
-        final Thread consumer = new Thread(
-                () -> {
-                    try {
-                        for (int i = 0; i != queueCall; i++) {
-                            System.out.println(queue.poll());
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                        Thread.currentThread().interrupt();
-                    }
-                }
-        );
         consumer.start();
-
     }
 }
